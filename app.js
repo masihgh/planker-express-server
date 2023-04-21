9a18b11180fffbcd0be5c32ec5278354b78a93ee
masihgh <masihprogrammer@gmail.com>
2023-04-22 01:25:47 +0330

    remove middlewares / add route comment in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const app = express()
-const {errorHandler,logErrors} = require('./middleware/Error')
+
 //Routes
 const HomeRoute = require('./routes/HomeRoute')
 
@@ -15,8 +15,6 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(errorHandler)
-app.use(logErrors)
 
 //Routes
 app.use('/', HomeRoute)
