5aad11354358569220ddcfba33832819101b6e28
masihgh <masihprogrammer@gmail.com>
2023-04-21 23:57:46 +0330

    define basic middlewares

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
