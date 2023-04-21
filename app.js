7cdf2e2924227a8ba31bdf31e7a955d8a875ee8d
masihgh <masihprogrammer@gmail.com>
2023-04-22 00:02:35 +0330

    use PORT from .env file

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const app = express()
-const port = 3000
 
+
+//Configs
+dotenv.config()
+const port = process.env.PORT || 5000
 
 //Middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-//Configs
-dotenv.config()
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
