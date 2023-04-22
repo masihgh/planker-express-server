9bf705c41a4664223ab0da976013d02b05a95a6b
masihgh <masihprogrammer@gmail.com>
2023-04-22 23:29:03 +0330

    add login route + change register method to POST

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { register } = require('../controllers/AuthController')
+const { register,login } = require('../controllers/AuthController')
 const { body } = require('express-validator');
 const { ValidatorMiddle } = require('../middlewares/ValidatorMiddle');
 
 const router = express.Router()
 
-router.get('/register', body('email').notEmpty(), body('username').notEmpty(), body('password').notEmpty(), body('first_name').notEmpty(), body('last_name').notEmpty(), ValidatorMiddle, register)
+router.post('/register', body('email').notEmpty(), body('username').notEmpty(), body('password').notEmpty(), body('first_name').notEmpty(), body('last_name').notEmpty(), ValidatorMiddle, register)
+router.post('/login', body('email').notEmpty(), body('password').notEmpty(), ValidatorMiddle, login)
 
 module.exports = router
