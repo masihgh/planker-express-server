f93331bdc5e5e9687cb7bdd067aa262b8700b0aa
masihgh <masihprogrammer@gmail.com>
2023-04-22 22:56:10 +0330

    add validation to route register

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { register } = require('../controllers/AuthController')
+const { body } = require('express-validator');
+const { ValidatorMiddle } = require('../middlewares/ValidatorMiddle');
 
 const router = express.Router()
 
-router.get('/register', register)
+router.get('/register', body('email').notEmpty(), body('username').notEmpty(), body('password').notEmpty(), body('first_name').notEmpty(), body('last_name').notEmpty(), ValidatorMiddle, register)
 
 module.exports = router
