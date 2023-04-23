e1d74ab99e005af5bef33ba83a58a016dd78c95f
masihgh <masihprogrammer@gmail.com>
2023-04-23 14:25:11 +0330

    fixed response

--- middlewares/ValidatorMiddle.js
+++ middlewares/ValidatorMiddle.js
@@ -1,11 +1,19 @@
-const { body,validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const ValidatorMiddle = async (req, res,next) => {
   const result = validationResult(req);
   if (result.isEmpty()) {
     next()
   }else{
-    return res.status(400).json({ errors: result.array() });
+    let messages = []
+    result.array().map((error) => {
+      messages.push(`${error.type} ${error.path} ${error.msg}`)
+    })
+    return res.status(400).json({
+      statusCode: 400,
+      error: 'Validation Error',
+      message: messages,
+    });
   }
 }
 
